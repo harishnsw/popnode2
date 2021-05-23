1b13f1bd849607aebfc202c874227feb86e3e230
Harish <78887571+harishnsw@users.noreply.github.com>
2021-05-23 17:27:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 var TinyDB = require("tinydb");
 mint_db = new TinyDB("./mint.db");
 app.use(express.json()); // built-in middleware for express
